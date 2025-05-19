60318b24821b719ca306ffb06a61e8a604e83e40
hoyun06 <hoyun06@gmail.com>
2025-05-20 00:13:24 +0900

    [HW2-2-2] BicycleCollection.h 생성

---
+++ HW2-2-2/BicycleCollection.h
@@ -0,0 +1,20 @@
+#ifndef BICYCLECOLLECION_H
+#define BICYCLECOLLECION_H
+
+#include <vector>
+
+#include "Bicycle.h"
+
+using namespace std;
+
+class BicycleCollection
+{
+private:
+	vector<Bicycle> bicycles;
+public:
+	void addBicycle(Bicycle bicycle);
+	vector<Bicycle> getAllbicycles();
+	Bicycle findBicycle(string bikeId);
+};
+
+#endif
