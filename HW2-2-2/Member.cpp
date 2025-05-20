b9ff2c7a9bd7a354b5dd3d48f7c95feb386b141a
hoyun06 <hoyun06@gmail.com>
2025-05-20 21:41:54 +0900

    [HW2-2-2] Member.cpp 수정

--- HW2-2-2/Member.cpp
+++ HW2-2-2/Member.cpp
@@ -7,14 +7,17 @@ Member::Member(string id, string password, string tel)
 	this->tel = tel;
 	this->loggedIn = false;
 }
+
 void Member::login()
 {
 	this->loggedIn = true;
 }
+
 void Member::logout()
 {
 	this->loggedIn = false;
 }
+
 vector<string> Member::getMemberDetail()
 {
 	vector<string> v;
@@ -22,14 +25,17 @@ vector<string> Member::getMemberDetail()
 	v.push_back(this->password);
 	return v;
 }
+
 bool Member::isLoggedIn()
 {
 	return this->loggedIn;
 }
+
 void Member::addRentedBicycles(Bicycle bicycle)
 {
 	this->rentedBicycleCollection.addBicycle(bicycle);
 }
+
 vector<Bicycle> Member::listRentedBicycles()
 {
 	return this->rentedBicycleCollection.getAllbicycles();
