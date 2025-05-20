30dad427d3bae3a8a706023374e8ffb7bf5d5e91
hoyun06 <hoyun06@gmail.com>
2025-05-20 13:28:46 +0900

    [HW2-2-2] MemberCollection.h 생성

---
+++ HW2-2-2/MemberCollection.h
@@ -0,0 +1,21 @@
+#ifndef MEMBERCOLLECTION_H
+#define MEMBERCOLLECTION_H
+
+#include <string>
+#include <vector>
+
+#include "Member.h"
+
+using namespace std;
+
+class MemberCollection
+{
+private:
+	vector<Member> members;
+public:
+	void addMember(Member member);
+	Member findMember(string id, string password);
+	vector<Member> getAllMembers();
+};
+
+#endif 
