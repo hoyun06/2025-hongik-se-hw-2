3d622510ada1be2d73353439696433360467dab0
hoyun06 <hoyun06@gmail.com>
2025-05-20 13:23:59 +0900

    [HW2-2-2] Member.h 생성

---
+++ HW2-2-2/Member.h
@@ -0,0 +1,28 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#include <string>
+#include <vector>
+
+#include "BicycleCollection.h"
+
+using namespace std;
+
+class Member
+{
+private:
+	string id;
+	string password;
+	string tel;
+	bool isLoggedIn;
+	BicycleCollection rentedBicycleCollection;
+public:
+	Member(string id, string password, string tel);
+	void login();
+	void logout();
+	string getId();
+	void addRentedBicycles(Bicycle bicycle);
+	vector<Bicycle> listRentedBicycles();
+};
+
+#endif 
