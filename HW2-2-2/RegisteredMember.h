b129071efb8f9dec11c71109504bf7729c9ed8c4
hoyun06 <hoyun06@gmail.com>
2025-05-20 13:33:16 +0900

    [HW2-2-2] RegisteredMember.h 생성

---
+++ HW2-2-2/RegisteredMember.h
@@ -0,0 +1,20 @@
+#ifndef REGISTEREDMEMBER_H
+#define REGISTEREDMEMBER_H
+
+#include <string>
+
+#include "MemberCollection.h"
+
+using namespace std;
+
+class RegisteredMember
+{
+private:
+	MemberCollection registeredMemberCollection;
+public:
+	void addMember(string id, string password, string tel);
+	Member findMember(string id, string password);
+	Member getLoggedInMember();
+};
+
+#endif 
