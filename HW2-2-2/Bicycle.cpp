ec0df719965d8abd6dc532d909deb9cd2007af34
hoyun06 <hoyun06@gmail.com>
2025-05-20 19:23:11 +0900

    [HW2-2-2] Bicycle.cpp 수정

--- HW2-2-2/Bicycle.cpp
+++ HW2-2-2/Bicycle.cpp
@@ -1,11 +1,13 @@
 #include "Bicycle.h"
 
-Bicycle::Bicycle(string bikeId, string name) {
+Bicycle::Bicycle(string bikeId, string name) 
+{
 	this->bikeId = bikeId;
 	this->name = name;
 }
 
-vector<string> Bicycle::getBicycleDetail() {
+vector<string> Bicycle::getBicycleDetail() 
+{
 	vector<string> v;
 	v.push_back(this->bikeId);
 	v.push_back(this->name);
