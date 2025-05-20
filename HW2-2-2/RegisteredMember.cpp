23a93fb20605fd4a0fa3d2eb5517cb4d5274d823
hoyun06 <hoyun06@gmail.com>
2025-05-20 21:53:25 +0900

    [HW2-2-2] RegisteredMember.cpp 생성

---
+++ HW2-2-2/RegisteredMember.cpp
@@ -0,0 +1,24 @@
+#include "RegisteredMember.h"
+
+void RegisteredMember::addMember(string id, string password, string tel)
+{
+	Member member(id, password, tel);
+	this->registeredMemberCollection.addMember(member);
+}
+
+Member RegisteredMember::findMember(string id, string password)
+{
+	return this->registeredMemberCollection.findMember(id, password);
+}
+
+Member RegisteredMember::getLoggedInMember()
+{
+	vector<Member> members = this->registeredMemberCollection.getAllMembers();
+	for (int i = 0; i < members.size(); i++)
+	{
+		if (members[i].isLoggedIn())
+		{
+			return members[i];
+		}
+	}
+}
