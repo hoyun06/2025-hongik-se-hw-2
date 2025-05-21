533e48ce98e9afe207642868f538a47d351982d0
hoyun06 <hoyun06@gmail.com>
2025-05-21 12:35:24 +0900

    [HW2-2-2] Logout.cpp 생성

---
+++ HW2-2-2/Logout.cpp
@@ -0,0 +1,14 @@
+#include "Logout.h"
+
+#include <vector>
+
+#include "LogoutUI.h"
+
+Logout::Logout(ofstream& outFp, RegisteredMember& members)
+{
+	this->logoutUI = new LogoutUI();
+	Member m = members.getLoggedInMember();
+	m.logout();
+	vector<string> v = m.getMemberDetail();
+	this->logoutUI->startInterface(outFp, v[0]);
+;}
