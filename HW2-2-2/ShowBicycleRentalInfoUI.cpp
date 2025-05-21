2005065d8a529d13d69935ed74987effb870a6ce
hoyun06 <hoyun06@gmail.com>
2025-05-21 22:23:35 +0900

    [HW2-2-2] 코드 문법 오류 수정

--- HW2-2-2/ShowBicycleRentalInfoUI.cpp
+++ HW2-2-2/ShowBicycleRentalInfoUI.cpp
@@ -3,7 +3,7 @@
 void ShowBicycleRentalInfoUI::startInterface(ofstream& outFp, vector<vector<string>>& bicycleDetails)
 {
 	outFp << "5.1. 자전거 대여 리스트" << '\n';
-	for (int i = 0; bicycleDetails.size(); i++)
+	for (int i = 0; i < bicycleDetails.size(); i++)
 	{
 		vector<string> v = bicycleDetails[i];
 		outFp << "> " << v[0] << " " << v[1] << '\n';
