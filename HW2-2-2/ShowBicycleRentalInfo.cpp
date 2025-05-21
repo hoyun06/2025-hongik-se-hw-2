971fddd952413ed7d80540cc8daed7c3ef3ee152
hoyun06 <hoyun06@gmail.com>
2025-05-21 13:30:39 +0900

    [HW2-2-2] ShowBicycleRentalInfo.cpp 생성

---
+++ HW2-2-2/ShowBicycleRentalInfo.cpp
@@ -0,0 +1,19 @@
+#include "ShowBicycleRentalInfo.h"
+
+#include <vector>
+
+#include "ShowBicycleRentalInfoUI.h"
+
+ShowBicycleRentalInfo::ShowBicycleRentalInfo(ofstream& outFp, RegisteredMember& members)
+{
+	this->showBicycleRentalInfoUI = new ShowBicycleRentalInfoUI();
+	Member m = members.getLoggedInMember();
+	vector<Bicycle> v = m.listRentedBicycles();
+
+	vector<vector<string>> bicycleDetails;
+	for (int i = 0; i < v.size(); i++)
+	{
+		bicycleDetails.push_back(v[i].getBicycleDetail());
+	}
+	this->showBicycleRentalInfoUI->startInterface(outFp, bicycleDetails);
+}
