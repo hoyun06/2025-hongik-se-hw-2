55765b27872ae77b087d721986eca5149c145849
hoyun06 <hoyun06@gmail.com>
2025-05-21 12:11:42 +0900

    [HW2-2-2] Login.cpp 생성

---
+++ HW2-2-2/Login.cpp
@@ -0,0 +1,14 @@
+#include "Login.h"
+#include "LoginUI.h"
+
+Login::Login(ofstream& outFp, ifstream& inFp, RegisteredMember& members)
+{
+	LoginUI loginUI(this);
+	loginUI.submitLoginInfo(outFp, inFp, members);
+}
+
+void Login::login(string id, string password, RegisteredMember& members)
+{
+	Member m = members.findMember(id, password);
+	m.login();
+}
