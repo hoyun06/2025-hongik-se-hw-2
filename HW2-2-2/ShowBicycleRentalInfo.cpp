6807342282a5dc1d8ef53bace8308f0ddacdf6ed
hoyun06 <hoyun06@gmail.com>
2025-05-23 21:53:15 +0900

    [HW2-2-2] ShowBicycleRentalInfo.cpp 수정

--- HW2-2-2/ShowBicycleRentalInfo.cpp
+++ HW2-2-2/ShowBicycleRentalInfo.cpp
@@ -1,6 +1,7 @@
 #include "ShowBicycleRentalInfo.h"
 
 #include <vector>
+#include <algorithm>
 
 #include "ShowBicycleRentalInfoUI.h"
 
@@ -9,6 +10,10 @@ ShowBicycleRentalInfo::ShowBicycleRentalInfo(ofstream& outFp, RegisteredMember& 
 	this->showBicycleRentalInfoUI = new ShowBicycleRentalInfoUI();
 	Member* m = members.getLoggedInMember();
 	vector<Bicycle> v = m->listRentedBicycles();
+
+	sort(v.begin(), v.end(), [](Bicycle& b1, Bicycle& b2) {
+		return b1.getBikeId() < b2.getBikeId();
+		});
 
 	vector<vector<string>> bicycleDetails;
 	for (int i = 0; i < v.size(); i++)
